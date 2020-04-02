8039d972ff51956bca3296b85c8eb63eb585a141
aksharp99 <aksharp99@gmail.com>
2020-04-02 13:43:11 -0400

    fixed a syntax error in the Stopwatch Component

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -2,13 +2,13 @@ import React, { Component } from "react";
 
 class Stopwatch extends Component {
   render() {
-    return(
-    <div className="stopwatch">
-      <h2>Stopwatch</h2>
-      <span className="stopwatch-time">0</span>
-      <button>Start</button>
-      <button>Reset</button>
-    </div>;
+    return (
+      <div className="stopwatch">
+        <h2>Stopwatch</h2>
+        <span className="stopwatch-time">0</span>
+        <button>Start</button>
+        <button>Reset</button>
+      </div>
     );
   }
 }
