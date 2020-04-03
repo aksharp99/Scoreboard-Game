e557db0799fc2612c95f1d08165de9bb34016ae4
aksharp99 <aksharp99@gmail.com>
2020-04-03 13:50:59 -0400

    Destructuring Props to make components cleaner and easier to understand

--- src/components/Player.js
+++ src/components/Player.js
@@ -2,24 +2,17 @@ import React, { PureComponent } from "react";
 import Counter from "./Counter";
 class Player extends PureComponent {
   render() {
-    console.log(this.props.name + " rendered");
+    const { name, id, score, index, removePlayer, changeScore } = this.props;
     return (
       <div className="player">
         <span className="player-name">
-          <button
-            className="remove-player"
-            onClick={() => this.props.removePlayer(this.props.id)}
-          >
+          <button className="remove-player" onClick={() => removePlayer(id)}>
             ✖
           </button>
-          {this.props.name}
+          {name}
         </span>
 
-        <Counter
-          score={this.props.score}
-          index={this.props.index}
-          changeScore={this.props.changeScore}
-        />
+        <Counter score={score} index={index} changeScore={changeScore} />
       </div>
     );
   }
